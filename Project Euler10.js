fc9f0c827f52c57b094f787aa1c2f7fe04523aed
0423yjh <0423yjh@gmail.com>
2018-08-10 17:49:52 +0900

    solution #10

--- Project Euler10.js
+++ Project Euler10.js
@@ -1,13 +1,33 @@
 /*
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-
 Find the sum of all the primes below two million.
 */
-function Prime(){
 
+/*
+10 이하의 소수를 모두 더하면 2 + 3 + 5 + 7 = 17 이 됩니다.
+이백만(2,000,000) 이하 소수의 합은 얼마입니까?
+*/
+
+function Prime(){
+  var a = 0;
+  var sum = 0;
+  while(true){
+      var k = 0;
+      for(var i = 0; i < a; i++){
+        if(a % i == 0){
+          k += 1;
+        }
+        if(k == 2){
+          sum += a;
+        }
+      }
+      if(a >= 2000000){
+        console.log(sum);
+        break;
+      }
+      a++
+      console.log(a);
+  }
 }
 
-alert(Prime());
-
-
-//not solve, pass
+Prime();
